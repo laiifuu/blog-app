3f3eb0b1d3b7501b5261d66f38925778dd9ae944
Hayet Fellah <hayet.fellah145@gmail.com>
2023-01-23 15:59:50 +0100

    Adjust routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "users#index"
-  
-  get '/', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get '/users/:id/posts', to: 'posts#index'
-  get '/users/:id/posts/:id', to: 'posts#show'
+  root "users#index"
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   
 end
