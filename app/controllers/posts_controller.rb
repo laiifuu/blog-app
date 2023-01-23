002143500eae8d5d407ca60d112b7a9541ddd6fb
Hayet Fellah <hayet.fellah145@gmail.com>
2023-01-23 19:16:45 +0100

    Remove queries and puts messages from the controllers actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,4 @@
 class PostsController < ApplicationController
   def show
-    user_id = params[:id].to_i
-    # puts :users[user_id].posts
-    @posts = Post.where(user_id == :author_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,7 @@
 class UsersController < ApplicationController
   def index
-    puts :users
   end
 
   def show
-    user_id = params[:id].to_i
-    puts User.find(user_id)
   end
 end
