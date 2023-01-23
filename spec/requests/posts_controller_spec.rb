2012dd47d1c7d0fc986542484ad32b28c877a8e2
Hayet Fellah <hayet.fellah145@gmail.com>
2023-01-23 18:16:35 +0100

    Fix linter errors

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "PostsControllers", type: :request do
-  describe "Posts  controller testing" do
-    it "GET /users/:id/posts" do
-      get "/users/:239/posts"
-      expect(response).to have_http_status(:ok)  
+RSpec.describe 'PostsControllers', type: :request do
+  describe 'Posts  controller testing' do
+    it 'GET /users/:id/posts' do
+      get '/users/:239/posts'
+      expect(response).to have_http_status(:ok)
       expect(response).to render_template('posts/index')
-      expect(response.body).to include("<h1>This is the view to show all the posts for a specific user</h1>")
+      expect(response.body).to include('<h1>This is the view to show all the posts for a specific user</h1>')
     end
 
-    it "GET /users/:id/posts/:id" do
-      get "/users/239/posts/178"
-      expect(response).to have_http_status(:ok)  
+    it 'GET /users/:id/posts/:id' do
+      get '/users/239/posts/178'
+      expect(response).to have_http_status(:ok)
       expect(response).to render_template('posts/show')
-      expect(response.body).to include("<h1>View to show a single post</h1>")
+      expect(response.body).to include('<h1>View to show a single post</h1>')
     end
   end
 end
