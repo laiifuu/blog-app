976652b53dc9b3aa4f139f882fb301b1141b6745
Hayet Fellah <hayet.fellah145@gmail.com>
2023-01-24 17:21:38 +0100

    Define controllers methods for users and posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
-  def show; end
+  def show
+    user_id = params[:id].to_i
+    @user = User.find(user_id)
+    @posts = @user.recent_posts
+  end
 end
