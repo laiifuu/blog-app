74ee3ebc4a7665c88b5bccfbe336a783ff409843
Hayet Fellah <hayet.fellah145@gmail.com>
2023-01-26 14:41:08 +0100

    Add new and create methods to the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     user_id = params[:user_id]
     @user = User.find(user_id)
     @posts = Post.where(author_id: user_id)
+    @recent_user = current_user
   end
 
   def show
@@ -11,5 +12,23 @@ class PostsController < ApplicationController
     @user = User.find(user_id)
     @post = Post.find(post_id)
     @comments = Comment.where(post_id:)
+    @recent_user = current_user
+  end
+
+  def new 
+    @post = Post.new
+  end
+
+  def create
+    user = self.current_user
+    puts user
+    @post = Post.new(
+      title: params[:post][:title], 
+      text: params[:post][:text], 
+      author: user
+    )
+    if @post.save
+      redirect_to user_posts_path
+    end
   end
 end
