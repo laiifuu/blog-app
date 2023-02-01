e46ca35d1087a148521107026e73ad249cb76ef9
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-01 15:16:07 +0100

    Remove current_user and add before_action helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user = User.all.first
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio])
   end
+
+  def index
+    
+  end 
 end
