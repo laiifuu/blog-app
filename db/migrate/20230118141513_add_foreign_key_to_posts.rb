ee1fdb89cca7d4c2336afc53040edd3200d561dd
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-02 14:05:09 +0100

    Add on-delete: cascade to the foreign keys

--- db/migrate/20230118141513_add_foreign_key_to_posts.rb
+++ db/migrate/20230118141513_add_foreign_key_to_posts.rb
@@ -1,5 +1,5 @@
 class AddForeignKeyToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :user, foreign_key: { to_table: :users }
+    add_reference :posts, :user, foreign_key: { to_table: :users }, on_delete: :cascade
   end
 end
