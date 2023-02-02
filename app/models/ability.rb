5c79dc2616932ad48b90d7eae43db71e4ff4c250
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-02 21:11:54 +0100

    Fix authorizations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Post, :all
-    can(%i[create destroy], Post, user:)
-    can(:destroy, Comment, user:)
-    can :manage, :all if user.role == 'admin'
+    can :read, Post
+
+    if user.role == "user"
+      can :create, Post
+      can :destroy, Post, author_id: user.id
+      can :create, Comment
+      can :destroy, Comment, author_id: user.id
+    elsif user.admin?
+      can :manage, :all
+    end
   end
 end
