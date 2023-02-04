14d573b03b889baa51fc898477dae9f01f3979e7
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-04 17:18:44 +0100

    Add spec file

---
+++ spec/requests/api/my_spec.rb
@@ -0,0 +1,19 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/my', type: :request do
+  it 'GET api/v1/posts' do
+    get user_api_v1_posts_path(1)
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'GET api/v1/posts' do
+    get user_api_v1_posts_path(1,1)
+    expect(response).to have_http_status(:ok)
+  end
+
+  # it 'GET api/v1/posts' do
+  #   post user_api_v1_posts_path(1)
+  #   expect(response).to have_http_status(:ok)
+  # end
+
+end
