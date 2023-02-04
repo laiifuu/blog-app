c89d971e2e3fb58dc20f4a90b0965af59636352b
Hayet Fellah <hayet.fellah145@gmail.com>
2023-02-04 12:57:32 +0100

    Split the methods into different controllers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,18 +1,3 @@
-class ApiController < ApplicationController
-  def return_posts
-    @posts = Post.where(author_id: params[:user_id])
-    render json: @posts
-  end
-
-  def return_comments
-    @comments = Comment.where(post_id: params[:post_id])
-    render json: @comments
-  end
-
-  def create_comment
-    text = params[:text]
-    post_id = params[:post_id]
-    comment = Comment.create(text:, author: current_user, post_id:)
-    render json: comment, status: 200
-  end
+class ApiController < ActionController::Base
+  protect_from_forgery with: :null_session
 end
